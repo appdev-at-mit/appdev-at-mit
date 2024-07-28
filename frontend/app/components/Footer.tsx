5124e9d2a98e1cfc9a1d76545e99e301671e30d8
Hailey <haileyrpan@gmail.com>
2024-07-28 13:53:45 -0400

    add footer

---
+++ frontend/app/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white bottom-0 left-0 right-0 z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-gray-700 text-center">Full Stack @ MIT © 2024.</p>
+          <p className="text-gray-700 text-center">
+            Made with <span className="text-red-600">❤️</span> by Hailey Pan.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
