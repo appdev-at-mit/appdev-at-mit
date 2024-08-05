b44e6f0f63e9de905b818d6fe49676815d96fd72
Hailey <haileyrpan@gmail.com>
2024-08-05 10:20:48 -0400

    First iteration of Teams page

---
+++ frontend/app/components/TeamMember.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+interface TeamMemberProps {
+  name: string;
+  roles: string[];
+  year: string;
+  imageSrc: string;
+}
+
+const DEFAULT_IMAGE_WIDTH = 300;
+const DEFAULT_IMAGE_HEIGHT = 300;
+
+const TeamMember: React.FC<TeamMemberProps> = ({
+  name,
+  roles,
+  year,
+  imageSrc,
+}) => {
+  return (
+    <div className="max-w-sm mx-auto bg-white border border-gray-300 rounded-lg shadow-md overflow-hidden">
+      <Image
+        src={imageSrc}
+        alt={name}
+        width={DEFAULT_IMAGE_WIDTH}
+        height={DEFAULT_IMAGE_HEIGHT}
+      />
+      <div className="p-4">
+        <h2 className="text-xl font-semibold text-gray-900">{name}</h2>
+        <div className="flex flex-wrap space-x-2 mt-2">
+          {roles.map((role, index) => (
+            <span
+              key={index}
+              className="inline-block bg-blue-200 text-blue-800 text-xs px-2 rounded-full"
+            >
+              {role}
+            </span>
+          ))}
+        </div>
+        <p className="text-gray-600 mt-4">Class of {year}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMember;
