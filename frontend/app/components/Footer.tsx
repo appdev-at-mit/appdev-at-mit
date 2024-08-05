a293a44cea3bd5f0c2a1cc0e191cfd242d349c37
Hailey <haileyrpan@gmail.com>
2024-08-05 11:58:22 -0400

    Update Footer.tsx

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -3,8 +3,8 @@ const Footer = () => {
     <footer className="bg-white bottom-0 left-0 right-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex flex-col justify-center items-center">
-          <p className="text-gray-700 text-center">Full Stack @ MIT © 2024.</p>
-          <p className="text-gray-700 text-center">
+          <p className="text-gray-500 text-center text-sm mb-1">Full Stack @ MIT © 2024.</p>
+          <p className="text-gray-500 text-center text-sm">
             Made with <span className="text-red-600">❤️</span> by Hailey, Josie, and Kaitlyn.
           </p>
         </div>
