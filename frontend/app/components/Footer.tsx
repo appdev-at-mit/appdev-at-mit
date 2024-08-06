ab148486ba024be7fb15c698c5bce15c36a11f6c
Hailey <haileyrpan@gmail.com>
2024-08-06 10:37:46 -0400

    add socials to footer

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,12 +1,27 @@
+import { FaInstagram, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+
 const Footer = () => {
   return (
     <footer className="bg-white bottom-0 left-0 right-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        <div className="flex flex-col justify-center items-center">
-          <p className="text-gray-500 text-center text-sm mb-1">Full Stack @ MIT © 2024.</p>
-          <p className="text-gray-500 text-center text-sm">
-            Made with <span className="text-red-600">❤️</span> by Hailey, Josie, and Kaitlyn.
-          </p>
+      <div className="max-w-8xl my-4 mx-8 px-4 sm:px-6 lg:px-8 py-4">
+        <div className="flex flex-col items-start sm:items-center sm:flex-row justify-between">
+          <div>
+            <p className="text-gray-500 text-left text-md mb-1">Full Stack @ MIT © 2024.</p>
+            <p className="text-gray-500 text-left text-sm">
+              Made with <span className="text-red-600">❤️</span> by Hailey, Josie, and Kaitlyn.
+            </p>
+          </div>
+          <div className="flex space-x-4 mt-4 sm:mt-0">
+            <a href="https://www.instagram.com/mitfullstack/" target="_blank" rel="noopener noreferrer">
+              <FaInstagram className="text-gray-500 hover:text-gray-900" size={24} />
+            </a>
+            <a href="https://www.linkedin.com/company/full-stack-at-mit/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="text-gray-500 hover:text-gray-900" size={24} />
+            </a>
+            <a href="mailto:contact@mitfullstack.org">
+              <FaEnvelope className="text-gray-500 hover:text-gray-900" size={24} />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Navbar = () => {
   return (
     <nav className="bg-white fixed top-0 left-0 right-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-8xl md:mx-8 mx-2 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-20 items-center">
           <div className="flex-shrink-0">
             <Link href="/" className="flex items-center">
