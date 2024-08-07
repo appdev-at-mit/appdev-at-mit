03aabc0380d119bb858489fd46aac5f41b6d2d0c
unknown <jxwang4053@gmail.com>
2024-08-07 12:19:09 -0400

    changed images

--- frontend/app/products/projectDetails.ts
+++ frontend/app/products/projectDetails.ts
@@ -23,20 +23,20 @@ export const featuredProjects = [
     description:
       "Know exactly what you're getting into when you sign up for a class. MIT Course Reviews compiles and brings you data from the past 10 years on just about every course and professor.",
     link: "",
-    imageUrl: "/images/housing.svg",
+    imageUrl: "/images/queue.svg",
   },
   {
     title: "CampusExplorer",
     description:
       "New to MIT? No need to wander around aimlessly! Use CampusExplorer to discover dorms, campus food options, and the best study spots.",
     link: "",
-    imageUrl: "/images/queue.svg",
+    imageUrl: "/images/housing.svg",
   },
   {
     title: "MIT Clubs",
     description:
       "There are too many clubs and organizations to keep track of. Browse MIT's full list of clubs, and filter by club size, focus, and recruiting status to find your fit.",
     link: "",
-    imageUrl: "/images/rideshare.svg",
+    imageUrl: "/images/site.svg",
   },
 ];
